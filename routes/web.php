e014136ba938e31e3dcecb55d7560bab72f141f6
ss-ch <93768042+ss-ch@users.noreply.github.com>
2021-12-22 13:53:07 +0100

    routes

--- routes/web.php
+++ routes/web.php
@@ -19,30 +19,86 @@ Route::get('/', function () {
 
 
 
+
+
+Route::get('/404', function () {
+    return view('library.404');
+});
+
+Route::get('/blog', function () {
+    return view('library.blog');
+});
+
+Route::get('/blog-detail', function () {
+    return view('library.blog-detail');
+});
+
+Route::get('/books-media-detail-v1', function () {
+    return view('library.books-media-detail-v1');
+});
+
+Route::get('/books-media-detail-v2', function () {
+    return view('library.books-media-detail-v2');
+});
+
+Route::get('/books-media-gird-view-v1', function () {
+    return view('library.books-media-gird-view-v1');
+});
+
+Route::get('/books-media-gird-view-v2', function () {
+    return view('library.books-media-gird-view-v2');
+});
+
+Route::get('/books-media-list-view', function () {
+    return view('library.books-media-list-view');
+});
+
+Route::get('/cart', function () {
+    return view('library.cart');
+});
+
+Route::get('/contact', function () {
+    return view('library.contact');
+});
+
+Route::get('/home-v2', function () {
+    return view('library.home-v2');
+});
+
+Route::get('/home-v3', function () {
+    return view('library.home-v3');
+});
+
+Route::get('/index-2', function () {
+    return view('library.index-2');
+});
+
 Route::get('/news-events-detail', function () {
     return view('library.news-events-detail');
 });
-
-
-
 
 Route::get('/news-events-list-view', function () {
     return view('library.news-events-list-view');
 });
 
-
-
+Route::get('/news-events-list-view', function () {
+    return view('library.news-events-list-view');
+});
 
 Route::get('/services', function () {
     return view('library.services');
 });
 
-
-
-
 Route::get('/signin', function () {
     return view('library.signin');
 });
+
+
+
+
+
+
+
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
