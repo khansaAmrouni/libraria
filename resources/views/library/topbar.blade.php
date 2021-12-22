ed1f29acea406fbcd2ee0d1f0c6fbd58f02243ba
ss-ch <93768042+ss-ch@users.noreply.github.com>
2021-12-22 14:39:14 +0100

    dynamic menu top

---
+++ resources/views/library/topbar.blade.php
@@ -0,0 +1,195 @@
+        <!-- Start: Header Section -->
+        <header id="header-v1" class="navbar-wrapper">
+            <div class="container">
+                <div class="row">
+                    <nav class="navbar navbar-default">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <div class="navbar-header">
+                                    <div class="navbar-brand">
+                                        <h1>
+                                            <a href="{{route('index')}}">
+                                                <img src="images/libraria-logo-v1.png" alt="LIBRARIA" />
+                                            </a>
+                                        </h1>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-9">
+                                <!-- Header Topbar -->
+                                <div class="header-topbar hidden-sm hidden-xs">
+                                    <div class="row">
+                                        <div class="col-sm-6">
+                                            <div class="topbar-info">
+                                                <a href="tel:+61-3-8376-6284"><i class="fa fa-phone"></i>+61-3-8376-6284</a>
+                                                <span>/</span>
+                                                <a href="mailto:support@libraria.com"><i class="fa fa-envelope"></i>support@libraria.com</a>
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-6">
+                                            <div class="topbar-links">
+                                                <a href="{{route('signin')}}"><i class="fa fa-lock"></i>Login / Register</a>
+                                                <span>|</span>
+                                                <div class="header-cart dropdown">
+                                                    <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+                                                        <i class="fa fa-shopping-cart"></i>
+                                                        <small>0</small>
+                                                    </a>
+                                                    <div class="dropdown-menu cart-dropdown">
+                                                        <ul>
+                                                            <li class="clearfix">
+                                                                <img src="images/header-cart-image-01.jpg" alt="cart item" />
+                                                                <div class="item-info">
+                                                                    <div class="name">
+                                                                        <a href="#">The Great Gatsby</a>
+                                                                    </div>
+                                                                    <div class="author"><strong>Author:</strong> F. Scott Fitzgerald</div>
+                                                                    <div class="price">1 X $10.00</div>
+                                                                </div>
+                                                                <a class="remove" href="#"><i class="fa fa-trash-o"></i></a>
+                                                            </li>
+                                                            <li class="clearfix">
+                                                                <img src="images/header-cart-image-02.jpg" alt="cart item" />
+                                                                <div class="item-info">
+                                                                    <div class="name">
+                                                                        <a href="#">The Great Gatsby</a>
+                                                                    </div>
+                                                                    <div class="author"><strong>Author:</strong> F. Scott Fitzgerald</div>
+                                                                    <div class="price">1 X $10.00</div>
+                                                                </div>
+                                                                <a class="remove" href="#"><i class="fa fa-trash-o"></i></a>
+                                                            </li>
+                                                            <li class="clearfix">
+                                                                <img src="images/header-cart-image-03.jpg" alt="cart item" />
+                                                                <div class="item-info">
+                                                                    <div class="name">
+                                                                        <a href="#">The Great Gatsby</a>
+                                                                    </div>
+                                                                    <div class="author"><strong>Author:</strong> F. Scott Fitzgerald</div>
+                                                                    <div class="price">1 X $10.00</div>
+                                                                </div>
+                                                                <a class="remove" href="#"><i class="fa fa-trash-o"></i></a>
+                                                            </li>
+                                                        </ul>
+                                                        <div class="cart-total">
+                                                            <div class="title">SubTotal</div>
+                                                            <div class="price">$30.00</div>
+                                                        </div>
+                                                        <div class="cart-buttons">
+                                                            <a href="{{route('cart')}}" class="btn btn-dark-gray">View Cart</a>
+                                                            <a href="{{route('checkout')}}" class="btn btn-primary">Checkout</a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="navbar-collapse hidden-sm hidden-xs">
+                                    <ul class="nav navbar-nav">
+                                        <li class="dropdown active">
+                                            <a data-toggle="dropdown" class="dropdown-toggle disabled" href="{{route('index')}}">Home</a>
+                                       <!--      <ul class="dropdown-menu">
+                                                <li><a href="{route('index-2')}}">Home V1</a></li>
+                                                <li><a href="{{route('home-v2')}}">Home V2</a></li>
+                                                <li><a href="{{route('home-v3')}}">Home V3</a></li>
+                                            </ul> -->
+                                        </li>
+                                        <li class="dropdown">
+                                            <a data-toggle="dropdown" class="dropdown-toggle disabled" href="{{route('books-media-list-view')}}">Books &amp; Media</a>
+                                            <ul class="dropdown-menu">
+                                                <li><a href="{{route('books-media-list-view')}}">Books &amp; Media List View</a></li>
+                                                <li><a href="{{route('books-media-gird-view-v1')}}">Books &amp; Media Grid View V1</a></li>
+<!--                                                 <li><a href="{{route('books-media-gird-view-v2')}}">Books &amp; Media Grid View V2</a></li>
+ -->                                                <li><a href="{{route('books-media-detail-v1')}}">Books &amp; Media Detail V1</a></li>
+<!--                                                 <li><a href="{{route('books-media-detail-v2')}}">Books &amp; Media Detail V2</a></li>
+ -->                                            </ul>
+                                        </li>
+                                        <li class="dropdown">
+                                            <a data-toggle="dropdown" class="dropdown-toggle disabled" href="{{route('news-events-list-view')}}">News &amp; Events</a>
+                                            <ul class="dropdown-menu">
+                                                <li><a href="{{route('news-events-list-view')}}">News &amp; Events List View</a></li>
+                                                <li><a href="{{route('news-events-detail')}}">News &amp; Events Detail</a></li>
+                                            </ul>
+                                        </li>
+                                        <li class="dropdown">
+                                            <a data-toggle="dropdown" class="dropdown-toggle disabled" href="#">Pages</a>
+                                            <ul class="dropdown-menu">
+                                                <li><a href="{{route('cart')}}">Cart</a></li>
+                                                <li><a href="{{route('checkout')}}">Checkout</a></li>
+                                                <li><a href="{{route('signin')}}">Signin/Register</a></li>
+                                                <li><a href="{{route('404')}}">404/Error</a></li>
+                                            </ul>
+                                        </li>
+                                        <li class="dropdown">
+                                            <a data-toggle="dropdown" class="dropdown-toggle disabled" href="{{route('blog')}}">Blog</a>
+                                            <ul class="dropdown-menu">
+                                                <li><a href="{{route('blog')}}">Blog Grid View</a></li>
+                                                <li><a href="{{route('blog-detail')}}">Blog Detail</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="{{route('services')}}">Services</a></li>
+                                        <li><a href="{{route('contact')}}">Contact</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="mobile-menu hidden-lg hidden-md">
+                            <a href="#mobile-menu"><i class="fa fa-navicon"></i></a>
+                            <div id="mobile-menu">
+                                <ul>
+                                    <li class="mobile-title">
+                                        <h4>Navigation</h4>
+                                        <a href="#" class="close"></a>
+                                    </li>
+                                    <li>
+                                        <a href="{{route('index-2')}}">Home</a>
+                                   <!--      <ul>
+                                            <li><a href="{{route('index-2')}}">Home V1</a></li>
+                                            <li><a href="{{route('home-v2')}}">Home V2</a></li>
+                                            <li><a href="{{route('home-v3')}}">Home V3</a></li>
+                                        </ul> -->
+                                    </li>
+                                    <li>
+                                        <a href="{{route('books-media-list-view')}}">Books &amp; Media</a>
+                                        <ul>
+                                            <li><a href="{{route('books-media-list-view')}}">Books &amp; Media List View</a></li>
+                                            <li><a href="{{route('books-media-gird-view-v1')}}">Books &amp; Media Grid View V1</a></li>
+<!--                                             <li><a href="{{route('books-media-gird-view-v2')}}">Books &amp; Media Grid View V2</a></li>
+ -->                                            <li><a href="{{route('books-media-detail-v1')}}">Books &amp; Media Detail V1</a></li>
+<!--                                             <li><a href="{{route('books-media-detail-v2')}}">Books &amp; Media Detail V2</a></li>
+ -->                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a href="{{route('news-events-list-view')}}">News &amp; Events</a>
+                                        <ul>
+                                            <li><a href="{{route('news-events-list-view')}}">News &amp; Events List View</a></li>
+                                            <li><a href="{{route('news-events-detail')}}">News &amp; Events Detail</a></li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a href="#">Pages</a>
+                                        <ul>
+                                            <li><a href="{{route('cart')}}">Cart</a></li>
+                                            <li><a href="{{route('checkout')}}">Checkout</a></li>
+                                            <li><a href="{{route('signin')}}">Signin/Register</a></li>
+                                            <li><a href="{{route('404')}}">404/Error</a></li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a href="{{route('blog')}}">Blog</a>
+                                        <ul>
+                                            <li><a href="{{route('blog')}}">Blog Grid View</a></li>
+                                            <li><a href="{{route('blog-detail')}}">Blog Detail</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="{{route('services')}}">Services</a></li>
+                                    <li><a href="{{route('contact')}}">Contact</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </header>
+        <!-- End: Header Section -->
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('library.index');
-});
+})->name('index');
 
 
 
@@ -23,79 +23,81 @@ Route::get('/', function () {
 
 Route::get('/404', function () {
     return view('library.404');
-});
+})->name('404');
 
 Route::get('/blog', function () {
     return view('library.blog');
-});
+})->name('blog');
 
 Route::get('/blog-detail', function () {
     return view('library.blog-detail');
-});
+})->name('blog-detail');
 
 Route::get('/books-media-detail-v1', function () {
     return view('library.books-media-detail-v1');
-});
+})->name('books-media-detail-v1');
 
 Route::get('/books-media-detail-v2', function () {
     return view('library.books-media-detail-v2');
-});
+})->name('books-media-detail-v2');
 
 Route::get('/books-media-gird-view-v1', function () {
     return view('library.books-media-gird-view-v1');
-});
+})->name('books-media-gird-view-v1');
 
 Route::get('/books-media-gird-view-v2', function () {
     return view('library.books-media-gird-view-v2');
-});
+})->name('books-media-gird-view-v2');
 
 Route::get('/books-media-list-view', function () {
     return view('library.books-media-list-view');
-});
+})->name('books-media-list-view');
 
 Route::get('/cart', function () {
     return view('library.cart');
-});
+})->name('cart');
 
 Route::get('/contact', function () {
     return view('library.contact');
-});
+})->name('contact');
 
 Route::get('/home-v2', function () {
     return view('library.home-v2');
-});
+})->name('home-v2');
 
 Route::get('/home-v3', function () {
     return view('library.home-v3');
-});
+})->name('home-v3');
 
 Route::get('/index-2', function () {
     return view('library.index-2');
-});
+})->name('index-2');
 
 Route::get('/news-events-detail', function () {
     return view('library.news-events-detail');
-});
+})->name('news-events-detail');
 
 Route::get('/news-events-list-view', function () {
     return view('library.news-events-list-view');
-});
+})->name('news-events-list-view');
 
 Route::get('/news-events-list-view', function () {
     return view('library.news-events-list-view');
-});
+})->name('news-events-list-view');
 
 Route::get('/services', function () {
     return view('library.services');
-});
+})->name('services');
 
 Route::get('/signin', function () {
     return view('library.signin');
-});
+})->name('signin');
 
 
 
-
+Route::get('/checkout', function () {
+    return view('library.checkout');
+})->name('checkout');
 
 
 
