1e2995b65013707b2a0bf5451c1d33881876448d
ss-ch <93768042+ss-ch@users.noreply.github.com>
2021-12-22 16:31:43 +0100

    site

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -13,7 +13,14 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                       Mon Profil
+                    </x-jet-nav-link>
+                </div>
+
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-jet-nav-link href="/" >
+                       Retour Au Site
                     </x-jet-nav-link>
                 </div>
             </div>
